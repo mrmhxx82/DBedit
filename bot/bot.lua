c834177e7c32ad4ba012729d919062c06a9c15ca
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-04-22 19:22:42 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {190840223},
     admin_users = {},
     disabled_channels = {}
   }
